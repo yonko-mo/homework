4bbc6f8d40d1054cb42f71baff40bd31cc01d1e0
yonko-mo <alsantel.ma@gmail.com>
2026-05-16 18:36:29 +0300

    comment out main function in question_4.dart in dart homework folder

--- dart/session_1_homework/lib/question_4.dart
+++ dart/session_1_homework/lib/question_4.dart
@@ -1,6 +1,6 @@
 /* How can you declare a variable in Dart to store a person's age and ensure it can
 only hold integer values? Write the code. */
 
-void main() {
-  int personAge;
-}
+// void main() {
+//   int personAge;
+// }
